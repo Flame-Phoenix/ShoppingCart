4f7427f9e8804d6e7dc22c2c4f1ca189f872ce05
FlamePhoenix <thanos6161@gmail.com>
2020-10-18 12:40:41 +0300

    heroku changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import productReducer from './productReducer';
 import {cartReducer} from './cartReducer';
+import {orderReducer} from './orderReducer';
 import {combineReducers} from 'redux';
 
 const allReducers = combineReducers({
     productReducer,
-    cartReducer
+    cartReducer,
+    orderReducer
 });
 
 export default allReducers;
